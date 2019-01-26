56e017506c2c0a193f5b6272dbb8fb64471c0bb5
carterwc <cacbcarter68@gmail.com>
2019-01-26 14:57:03 -0500

    updated server

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json())
 // set te port number to be used localhost for testing
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
